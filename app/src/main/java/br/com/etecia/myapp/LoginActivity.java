a3a34ad92358384df0d0bc62f8852344d4015111
lndsilva <lndsilva@hotmail.com>
2025-02-12 21:13:47 -0300

    login

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -1,6 +1,7 @@
 package br.com.etecia.myapp;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +9,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+
 public class LoginActivity extends AppCompatActivity {
+    TextInputEditText txtEmail, txtSenha;
+    TextView lblEsqueceuSenha;
+    MaterialButton btnEntrar, btnCadastrarSe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,11 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        txtEmail = findViewById(R.id.txtEmail);
+        txtSenha = findViewById(R.id.txtSenha);
+        lblEsqueceuSenha = findViewById(R.id.lblEsqueceuSenha);
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnCadastrarSe = findViewById(R.id.btnCadastrarSe);
     }
 }
