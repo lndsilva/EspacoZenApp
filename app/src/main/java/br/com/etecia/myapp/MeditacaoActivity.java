f4f4dda58ae6e3e07eee0ab6326815e918067f9d
lndsilva <lndsilva@hotmail.com>
2025-02-19 21:59:46 -0300

    recycler meditacao

--- app/src/main/java/br/com/etecia/myapp/MeditacaoActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MeditacaoActivity.java
@@ -7,12 +7,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MeditacaoActivity extends AppCompatActivity {
 
     private List<Meditacao> lstMeditacao;
+    RecyclerView recMeditacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,59 @@ public class MeditacaoActivity extends AppCompatActivity {
             return insets;
         });
 
-        AdapterMeditacao adapterMeditacao = new AdapterMeditacao(getApplicationContext(),lstMeditacao);
+        lstMeditacao = new ArrayList<>();
+
+        lstMeditacao.add(
+                new Meditacao("Para repousar",
+                        "Meditação - 10 minutos", "string/meditar_texto",
+                        R.drawable.zen
+                )
+
+        );
+        lstMeditacao.add(
+                new Meditacao("Para repousar",
+                        "Meditação - 10 minutos", "string/meditar_texto",
+                        R.drawable.zen
+                )
+
+        );
+        lstMeditacao.add(
+                new Meditacao("Para repousar",
+                        "Meditação - 10 minutos", "string/meditar_texto",
+                        R.drawable.zen
+                )
+
+        );
+        lstMeditacao.add(
+                new Meditacao("Para repousar",
+                        "Meditação - 10 minutos", "string/meditar_texto",
+                        R.drawable.zen
+                )
+
+        );
+        lstMeditacao.add(
+                new Meditacao("Para repousar",
+                        "Meditação - 10 minutos", "string/meditar_texto",
+                        R.drawable.zen
+                )
+
+        );
+
+
+        recMeditacao = findViewById(R.id.recMeditacao);
+
+        AdapterMeditacao adapterMeditacao = new AdapterMeditacao(getApplicationContext(), lstMeditacao);
+
+        //Carregando o adaptador a lista
+
+        recMeditacao.setLayoutManager(
+                new LinearLayoutManager(getApplicationContext(),
+                        LinearLayoutManager.HORIZONTAL,true));
+
+        recMeditacao.setHasFixedSize(true);
+
+        recMeditacao.setAdapter(adapterMeditacao);
+
+
     }
 }
