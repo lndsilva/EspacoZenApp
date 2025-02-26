4c409beb5fd7571e5c47d8ccd7d10083fba41997
lndsilva <lndsilva@hotmail.com>
2025-02-26 19:24:08 -0300

    fragment adapter

--- app/src/main/java/br/com/etecia/myapp/BemEstarActivity.java
+++ app/src/main/java/br/com/etecia/myapp/BemEstarActivity.java
@@ -7,8 +7,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class BemEstarActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    AdapterBemEstar adapterBemEstar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,37 @@ public class BemEstarActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        tabLayout = findViewById(R.id.tblBemEstar);
+        viewPager2 = findViewById(R.id.vpgBemEstar);
+
+        adapterBemEstar = new AdapterBemEstar(this);
+
+        viewPager2.setAdapter(adapterBemEstar);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.getTabAt(position).select();
+            }
         });
     }
 }
